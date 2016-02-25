7027076581b496f72551274a5838721326f12583
Alexandr <sedachov@gmail.com>
2016-02-25 14:04:50 +0300

    Added files via upload

---
+++ ConvertToShapes.jsx
@@ -0,0 +1,14 @@
+﻿var comp = app.project.activeItem;
+var layers = comp.selectedLayers;
+var n = layers.length;
+
+for(var i = 0; i < n; i++)
+{
+    for(var j = 1; j <= comp.numLayers; j++)
+        comp.layer(j).selected = false;
+    layers[i].selected = true;   
+    app.executeCommand(3973); 
+    comp.layer(1).moveBefore(layers[i]);
+    layers[i].remove();
+}
+
