a006f0f3d15230929095073cb0bcc120068fb230
Alexandr <sedachov@gmail.com>
2016-07-01 18:48:55 +0300

    Add files via upload

---
+++ StrokeRound.jsx
@@ -0,0 +1,12 @@
+﻿var layers = app.project.activeItem.selectedLayers;
+
+for(var j = 0; j < layers.length; j++)
+    if(layers[j] instanceof ShapeLayer) RoundStrokes(layers[j].property("ADBE Root Vectors Group"));    
+
+function RoundStrokes(vectorGroup){
+    for(var i = 1; i <= vectorGroup.numProperties; i++){
+        if(vectorGroup.property(i).matchName == "ADBE Vector Stroke Line Cap")vectorGroup.property(i).setValue(2); 
+        if(vectorGroup.property(i).matchName == "ADBE Vector Stroke Line Join")vectorGroup.property(i).setValue(2);
+        else if(vectorGroup.property(i) instanceof PropertyGroup) RoundStrokes(vectorGroup.property(i));      
+    }
+}
